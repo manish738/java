1878aab4c0959e7f2d8dde964adb03454a9d9c40
manish738 <108449899+manish738@users.noreply.github.com>
2022-07-05 12:09:18 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,24 @@
+class Typecasting
+	{
+	public static void main(String args[])
+		{
+		char letter='A';
+		int ascii=letter;
+		char alp=(char)ascii;
+		System.out.println("int value lettter: "+ascii);
+		System.out.println("char value letter: "+alp);
+		}
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
