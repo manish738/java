4be7d28e8333490fbe268bf196025bf35f242da3
manish738 <108449899+manish738@users.noreply.github.com>
2022-07-12 11:50:38 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators
+{
+public static void main(String args[])
+	{
+	int a=18, b=7;
+//arthmetic operator
+System.out.println("a+b:" +(a+b));
+System.out.println("a-b:" +(a-b));
+System.out.println("a*b:" +(a*b));
+System.out.println("a/b:" +(a/b));
+System.out.println("a%b:" +(a%b));
+
+	}
+}
