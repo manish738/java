c3a098536984921a3991aaf17f5db8ed9ddd87aa
manish738 <108449899+manish738@users.noreply.github.com>
2022-07-28 11:26:18 +0530

    Add files via upload

---
+++ SingleDimensionDemo.java
@@ -0,0 +1,20 @@
+/*Write the same programme by assigningthearray 
+integer values 25, 89 and 100 for the class SingleDimensionDemo.*/
+
+class SingleDimensionDemo
+{
+	public static void main(String args[])
+	{
+	int [] a = new int[3];
+	a[0]=25;
+	a[1]=89;
+	a[2]=100;
+	System.out.println("Single dimension array elements are:");
+	System.out.println(a[0]);
+	System.out.println(a[1]);
+	System.out.println(a[2]);
+
+	}
+
+
+}
